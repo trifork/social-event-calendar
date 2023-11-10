c0c70e493165d91d971ec9888cc6df765ec7cba8
Tim Hjermitslev Keren Gjøderum <6584419+lajtmaN@users.noreply.github.com>
2023-11-10 21:02:44 +0100

    removed year param as it didn't make sense

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,10 +2,7 @@ import type { HeadersFunction, LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { Calendar } from "~/components/Calendar";
-import {
-  filterByUpcomingPredicate,
-  filterByYearPredicate,
-} from "~/services/calendar-filtering";
+import { filterByUpcomingPredicate } from "~/services/calendar-filtering";
 import { getCacheTime, getICSCalendarLink } from "~/services/calendar-ics-link";
 import { getCalendarEvents } from "~/services/calendar-parser";
 
@@ -18,26 +15,23 @@ export const headers: HeadersFunction = ({ loaderHeaders }) => {
 export const loader = async ({ params, request }: LoaderArgs) => {
   const events = await getCalendarEvents();
   const url = new URL(request.url);
-  const year =
-    Number(url.searchParams.get("year")) || new Date(Date.now()).getFullYear();
-  const months = Number(url.searchParams.get("upcomming")) || 3;
+
+  const upcomingMonths = Number(url.searchParams.get("upcoming")) || 3;
   const title = url.searchParams.get("title") || "Sociale Events";
 
-  const filtered = events
-    .filter(filterByYearPredicate(year))
-    .filter(filterByUpcomingPredicate(months));
+  const filtered = events.filter(filterByUpcomingPredicate(upcomingMonths));
 
   return json(
     {
       title,
-      subtitle: `Eventkalender næste ${months} mdr.`,
+      subtitle: `Eventkalender næste ${upcomingMonths} mdr.`,
       events: filtered,
       link: getICSCalendarLink(),
     },
     { headers: { "Cache-Control": `public, max-age=${getCacheTime()}` } }
   );
 };
-export default function YearlyCalendar() {
+export default function DefaultCalendar() {
   const { events, title, subtitle, link } = useLoaderData<typeof loader>();
   return (
     <Calendar
